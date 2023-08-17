bc87b998988ae6ade9358967e859491db9cc94b4
Umberto Fontanazza <umbertofontanazza@MacBook-Pro-di-Umberto.local>
2023-08-17 13:44:58 +0200

    Moved assets into separate folder

--- image_manager.py
+++ image_manager.py
@@ -6,8 +6,8 @@ class ImageManager:
     @classmethod
     def open_key_image(cls, char: str) -> ImageTk.PhotoImage:
         # TODO: generalize for any keyboard key
-        path = 'letter_a.png' if char == 'a' else None
-        path = 'letter_b.png' if char == 'b' else path
+        path = './assets/letter_a.png' if char == 'a' else None
+        path = './assets/letter_b.png' if char == 'b' else path
         if not path: raise(ValueError(f"Couldn't find a path for letter {char}"))
         image = Image.open(path)
         photo_image = ImageTk.PhotoImage(image)
