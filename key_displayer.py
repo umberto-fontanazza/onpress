e2313fe05ee33b1eef1a7c5c9c1bf39ad0464060
Umberto Fontanazza <umbertofontanazza@MacBook-Pro-di-Umberto.local>
2023-08-22 11:26:46 +0200

    Changed root window transparency

--- key_displayer.py
+++ key_displayer.py
@@ -22,6 +22,8 @@ class KeyDisplayer:
         self.__labels: dict[Union[keyboard.Key, keyboard.KeyCode], ttk.Label] = {}
         window.overrideredirect(True)
         window.geometry(f'0x{self.height}-100+100')
+        window.wm_attributes('-transparent', True)
+        window.configure(bg='systemTransparent')
         self.__initialized = True
 
     def start(self):
@@ -75,73 +77,3 @@ class KeyDisplayer:
             Timer(delay_seconds, hide_key_immediately).start()
         else:
             hide_key_immediately()
-
-    """
-    def __init__(self):
-        if self.__initialized: return
-        self.__shown_keys = 0
-        self.__init_tk_window()
-        self.__initialized = True
-
-    def __init_tk_window(self):
-        self.__window = tk.Tk()
-        window = self.__window
-        window.overrideredirect(True) # remove OS controls
-        window.rowconfigure(0, weight=1)
-        window.geometry('0x50-100+100')
-        window.attributes('-alpha', 0)
-        self.__init_tk_style()
-        listener = keyboard.Listener(on_press=self.show_char)
-        listener.start()
-        window.mainloop()
-
-    def __init_tk_style(self):
-        self._style = ttk.Style(self.__window)
-        self._style.theme_use('classic') # https://stackoverflow.com/questions/23750141/tkinter-ttk-widgets-ignoring-background-color
-        self._style.configure('TFrame', background='green')
-
-    def __create_image_frame(self, parent, image : tk.PhotoImage) -> ttk.Frame:
-        frame = ttk.Frame(parent)
-        canvas = tk.Canvas(frame, borderwidth=0, highlightthickness=0)
-        canvas.grid(row=0, column=0, sticky='nswe')
-        canvas.create_image((0,0), image=image, anchor='nw')
-        return frame
-
-    def show_char(self, key: Union[keyboard.Key, keyboard.KeyCode, None]):
-        try:
-            image : ImageTk.PhotoImage = ImageManager.open_key_image(key)
-        except ImageAlreadyOpened as already_opened:
-            print(already_opened)
-            return
-        except FileNotFoundError as fnf:
-            print(fnf)
-            return
-        except ValueError as ve:
-            print(ve)
-            return
-        self.__update_window_width(image.width())
-        self.__window.columnconfigure(self.__shown_keys, weight=1)
-        frame = self.__create_image_frame(self.__window, image)
-        frame.grid(row=0, column=self.__shown_keys, sticky='nswe')
-        Timer(2.0, lambda: self.hide_char(frame, key)).start()
-        self.__shown_keys += 1
-        if self.__shown_keys == 1:
-            self.__window.attributes('-alpha', 1) # show window
-
-    def hide_char(self, frame: ttk.Frame, key: Union[keyboard.Key, keyboard.KeyCode, None]):
-        frame.grid_forget()
-        frame.destroy()
-        image_width : int = ImageManager.get_opened_image(key).width()
-        self.__update_window_width(-image_width)
-        ImageManager.close_image(key)
-        self.__shown_keys -= 1
-        if self.__shown_keys == 0:
-            self.__window.attributes('-alpha', 0) # hide window
-
-    def __update_window_width(self, increment: int):
-        window = self.__window
-        old_width: int = window.winfo_width()
-        new_width = old_width + increment
-        new_width = new_width if new_width >= 0 else 0
-        window.geometry(f'{new_width}x50')
-    """
